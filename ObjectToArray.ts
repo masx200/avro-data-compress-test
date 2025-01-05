72bf8ea6965d353aab775df1ab57630a34c57907
masx200 <masx200@qq.com>
2025-01-05 11:57:10 +0800

    Update ObjectToArray.ts

--- ObjectToArray.ts
+++ ObjectToArray.ts
@@ -5,7 +5,7 @@ export function ObjectToArray(
 ): (Buffer<Uint8Array>)[] {
     const array: (Buffer<Uint8Array>)[] = [];
     array.length = Object.keys(arg0).length;
-    array.fill(Buffer.alloc(0));
+    array.fill(Buffer.from([]));
     arg0.forEach((key) => {
         array[Number(key[0])] = Buffer.from(key[1]);
     });
